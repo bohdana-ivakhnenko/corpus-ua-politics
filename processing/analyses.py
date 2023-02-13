05e76ab79ef9861366986f098f25bd1b9c70644a
Nika Babych <nanoornika@gmail.com>
2023-02-13 19:09:21 +0200

    Add basic structure

--- processing/analyses.py
+++ processing/analyses.py
@@ -1,14 +1,54 @@
-def get_text(name):
-    pass
+import stanza
+import pymorphy3
+import tokenize_uk
+# треба вирішити чим саме користуватимемось. stanza доволі повільна, але можна подивитись які будуть обсяги
+# імпортувати штуки для гарного виведення у консоль - кольори й таблички
+# імпортувати штуки для збереження - БД?
 
 
-class Analyses:
+class Analysis:
     def __init__(self, name):
         self.name = name
-        self.text = get_text(name)
+        self.read()
+        self.tokenize()
 
-    def rule(self):
+    def read(self):
+        # структура: папка із прізвищем-ім'ям, у ній папки old і new, у них кожен текст в окремому файлі
+        # окремий файл дасть можливість аналізувати частотність певних явищ на пост
+        # можна зробити систему із назва файлу - дата і час посту, аналізувати кількість постів на день тощо
+        # але якщо чесно мені не хочеться заморачуватись, а чи буде цим займатись хтось інший?
+        # або простіше: папка, у ній два файли, у файлах тексти із певним розділювачем (/n/n?)
+
+        # self.text = суцільний текст
+        # self.posts = окремі пости
+        pass
+
+    def tokenize(self):
+        # начебто тільки речення й слова треба
+        # чи треба зберігати набори слів/речень під конкретні пости чи можна суцільний текст?
+
+        # self.sents =
+        # self.words =
+        pass
+
+    def full(self):
+        # тут викликати всі інші функції аналізу
+        pass
+
+    def analysis_name(self):
+        # тіло аналізу
+
+        # if *property* in *text*:
+        #    self.analysis_name_result = "результат аналізу"
+        #    return self.analysis_name_result
+        #
+        # self.analysis_name_result = False
         pass
 
     def save_results(self):
+        # у якому форматі будемо зберігати? чи треба це взагалі?
         pass
+
+
+if __name__ == __main__:
+    politician = Analysis("volodymyr zelenskyi")
